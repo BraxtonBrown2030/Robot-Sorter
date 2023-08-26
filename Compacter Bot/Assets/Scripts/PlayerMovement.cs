7147cbb46851def4091a426fa2aff56f6aa7ce0f
Braxton Brown <10944268@uvu.edu>
2023-08-25 23:04:41 -0600

    trying line render movement and smoothing

--- Compacter Bot/Assets/Scripts/PlayerMovement.cs
+++ Compacter Bot/Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,42 @@ public class PlayerMovement : MonoBehaviour
 {
     
     public GameObject pickup;
+    public LineRenderer lineRenderer;
+    public bool lineRendererON;
+    public Camera sceneCamera;
+    public float moveSpeed;
+    public List<Transform> lineTransforms;
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+
+            Ray myRay = sceneCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit mayRayCastHit;
+
+            if(Physics.Raycast(myRay, out mayRayCastHit))
+            {
+
+                
+
+            }
+
+        }
+
+        if(lineRendererON == true)
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, lineRenderer., moveSpeed * Time.deltaTime);
+
+        }
+
     }
 }
