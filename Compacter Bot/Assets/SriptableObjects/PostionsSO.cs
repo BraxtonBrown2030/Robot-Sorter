9044933acda6095f2d1487e3b1544a471074e36d
Braxton Brown <10944268@uvu.edu>
2023-09-03 21:06:41 -0600

    Level postions So and Save foldes and submoudels

---
+++ Compacter Bot/Assets/SriptableObjects/PostionsSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PostionsSO : ScriptableObject
+{
+
+
+    public Vector3 postion;
+
+}
